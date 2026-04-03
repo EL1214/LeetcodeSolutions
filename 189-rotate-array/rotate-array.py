1eb273dded04d42fde53a04029303c322e2b2b8f
LaWunnHtet_El <125852819+EL1214@users.noreply.github.com>
2026-04-03 13:33:07 +0700

    Time: 7 ms (57.70%) | Memory: 25.4 MB (9.34%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -6,8 +6,11 @@ class Solution(object):
             new.append(nums[i])            
         for j in range(0,len(nums)-k):
             new.append(nums[j])
-        nums[:]= new #otherwise the order in the original array wont change 
+        nums[:]= new #keep the content but change the order 
         return new
        
+       #efficient one
+'''k = k % len(nums) #for when k is larger than the length of the array
+num[:]= num[-k:] + num[:-k]'''
         
         
