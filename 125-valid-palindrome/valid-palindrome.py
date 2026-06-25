62ad0e7b37ae1144385da70c156ededaecadcfd4
LaWunnHtet_El <125852819+EL1214@users.noreply.github.com>
2026-06-25 16:36:20 +0700

    Time: 21 ms (41.34%) | Memory: 12.6 MB (77.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,20 +1,13 @@
 class Solution(object):
     def isPalindrome(self, s):
-        #Convert
-        s = s.lower()
-        news = ""
-        for i in s:
-            if i.isalnum():
-                news += i
-        
-        Spointer = 0
-        Epointer = len(news)-1
-        while Spointer <= Epointer:
-            if news[Spointer] != news[Epointer]:
-                return False
-            else:
-                Spointer += 1
-                Epointer -= 1
-        return True
+        start , end = 0, len(s)-1
 
- 
+        while start <= end:
+            while start < end and not s[start].isalnum():
+                start += 1
+            while end > start and not s[end].isalnum():
+                end -= 1
+            if s[start].lower() != s[end].lower():
+                return False
+            start, end = start +1, end -1
+        return True
